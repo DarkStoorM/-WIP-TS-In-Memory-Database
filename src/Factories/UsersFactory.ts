99cd40de7a9009b32a92eab2d1fdad60ed098106
DarkStoorM <b.sieprawski@op.pl>
2022-12-17 01:15:24 +0100

    Add Users factory

---
+++ src/Factories/UsersFactory.ts
@@ -0,0 +1,30 @@
+import { Factory } from "../Base/Factory";
+import { IUser, UserModel, UsersDB } from "../Models/User";
+import { faker } from "@faker-js/faker";
+
+class FactoryOfUsers extends Factory<IUser, UserModel> {
+  public constructor() {
+    super(
+      (): IUser => ({
+        password: faker.internet.password(),
+        username: faker.internet.userName(),
+        banned: false,
+      }),
+      UsersDB
+    );
+  }
+
+  public banned(): this {
+    this.state({ banned: false });
+
+    return this;
+  }
+
+  public unnamed(): this {
+    this.state({ username: "" });
+
+    return this;
+  }
+}
+
+export const UsersFactory = new FactoryOfUsers();
